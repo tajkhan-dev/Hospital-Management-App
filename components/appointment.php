819121992a3873d0a06d91cba7157876d3f16c51
tajkhan-dev <bbadshah691@gmail.com>
2023-10-20 17:50:51 +0500

    appointment table connected

--- components/appointment.php
+++ components/appointment.php
@@ -1,6 +1,4 @@
-
-
-
+<?php include("connection.php")?>
 <section class="section appoinment">
     <div class="container">
         <div class="row align-items-center">
@@ -16,32 +14,31 @@
                 <div class="appoinment-wrap mt-5 mt-lg-0">
                     <h2 class="mb-2 title-color">Book appoinment</h2>
                     <p class="mb-4">Mollitia dicta commodi est recusandae iste, natus eum asperiores corrupti qui velit . Iste dolorum atque similique praesentium soluta.</p>
-                    <form id="#" class="appoinment-form" >
+                    <form id="#" class="appoinment-form" method="post" action="#">
                         <div class="row">
                             <div class="col-lg-6">
                                 <div class="form-group">
-                                    <form method="">
-                                    <select class="form-control" id="departselect" >
-                                       
+                                   
+                                        <select class="form-control" id="departselect" selected>
+
                                             <option value="">Select your country</option>
 
-                                    
-                                      
-                                        
-                                        
-                                     
-                                    </select>
-                                    </form>
+
+
+
+
+
+                                        </select>
+                              
 
                                 </div>
                             </div>
                             <div class="col-lg-6">
                                 <div class="form-group">
-                                    <form">
-   <select class="form-control" id="exampleFormControlSelect2">
-    
+                                    <select name="doc" class="form-control" id="docselect">
+                                            <option value="">Select Doctor</option>
                                         </select>
-                                    </form>
+                                 
                                 </div>
                             </div>
 
@@ -50,21 +47,9 @@
                                     <input name="date" id="date" type="text" class="form-control" placeholder="dd/mm/yyyy">
                                 </div>
                             </div>
-
                             <div class="col-lg-6">
                                 <div class="form-group">
-                                    <input name="time" id="time" type="text" class="form-control" placeholder="Time">
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="form-group">
-                                    <input name="name" id="name" type="text" class="form-control" placeholder="Full Name">
-                                </div>
-                            </div>
-
-                            <div class="col-lg-6">
-                                <div class="form-group">
-                                    <input name="phone" id="phone" type="Number" class="form-control" placeholder="Phone Number">
+                                    <input name="myname" id="name" type="text" class="form-control" placeholder="enter your name">
                                 </div>
                             </div>
                         </div>
@@ -72,8 +57,27 @@
                             <textarea name="message" id="message" class="form-control" rows="6" placeholder="Your Message"></textarea>
                         </div>
 
-                        <a class="btn btn-main btn-round-full" href="appoinment.html">Make Appoinment <i class="icofont-simple-right ml-2  "></i></a>
+
+<input type="submit" value="submit" name="submit">
+
                     </form>
+                    <?php 
+                    if(isset($_POST['submit'])){
+
+                        $name=$_POST['myname'];
+                        $message=$_POST['message'];
+                        $date=$_POST['date'];
+                        $docid=$_POST['doc'];
+
+                        $sql="INSERT INTO `appointments` (`patient_name`,`message`,`date`,`docid`)VALUES('$name','$message','$date','$docid')";
+                        $result=$conn->query($sql);
+
+                        echo "<script>
+                        alert('appointment scheduled')
+                        window.location.href='index.php'
+                        </script>";
+                    }                    
+                    ?>
                 </div>
             </div>
         </div>
@@ -83,29 +87,36 @@
 
 
 <script>
-$(document).ready(function($){
-    function loadDepartment(){
-        alert("I'm working");
-        $.ajax({
-            url:"getdept.php",
-            type: "POST",
-            success: function(data) {
-              
-                $("#departselect").append(data);
-            },
-            error: function(xhr, status, error) {
-                console.log("AJAX Error: " + error);
-            }
-        });
-    }
-    loadDepartment();
-});
+    $(document).ready(function($) {
+        function loadDepartment(type, deptid) {
 
+            $.ajax({
+                url: "getdept.php",
+                type: "POST",
+                data: {
+                    type: type,
+                    id: deptid
+                },
+                success: function(data) {
+                    if (type === "doc") {
 
+                        $("#docselect").html(data);
+                    } else {
+                        $("#departselect").append(data);
 
+                    }
+                },
+                error: function(xhr, status, error) {
+                    console.log("AJAX Error: " + error);
+                }
+            });
+        }
+        loadDepartment();
+        $("#departselect").on("change", function() {
+            var dept = $("#departselect").val()
 
+            loadDepartment("doc", dept)
 
-
-
-
+        })
+    });
 </script>
--- getdept.php
+++ getdept.php
@@ -1,16 +1,31 @@
 <?php 
 include("connection.php") ;
 
+if($_POST['type']!=="doc"){
 
-$query="SELECT * from `departments`";
+    $query="SELECT * from `departments`";
+    
+    $result=$conn->query($query);
+    $str="";
+    
+    
+    while($row=mysqli_fetch_assoc($result)){
+    
+        $str .="<option value={$row['deptid']}>{$row['deptname']}</option>";
+    }
+    echo $str;
+}else{
+    $query="SELECT * from `doctors` where deptid={$_POST['id']}";
+    
+    $result=$conn->query($query);
+    $str="";
+    
+    
+    while($row=mysqli_fetch_assoc($result)){
+    
+        $str .="<option value={$row['docid']}>{$row['docname']}</option>";
+    }
+    echo $str;
 
-$result=$conn->query($query);
-$str="";
-
-
-while($row=mysqli_fetch_assoc($result)){
-
-    $str .="<option value={$row['deptid']}>{$row['deptname']}</option>";
 }
-echo $str;
 ?>
